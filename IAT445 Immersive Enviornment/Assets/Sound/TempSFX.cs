9e58dec8fc2bd45817e2949cdada6cc5af699768
Parodeen <kjl19@sfu.ca>
2025-11-16 18:53:39 -0800

    UI fix
    also tried to add sound to level 1

---
+++ IAT445 Immersive Enviornment/Assets/Sound/TempSFX.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class TempSFX : MonoBehaviour
+{
+    [SerializeField] private AudioClip doorSFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SFX()
+    {
+        SoundManager.instance.PlaySFX(doorSFX, transform, 1f);
+    }
+}
